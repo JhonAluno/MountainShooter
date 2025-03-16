2418ef4c4e533b1fffeb0239c2e2d7757e55524b
JhonJn <3848325@alunouninter.com>
2025-03-16 20:15:36 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 Bg implementation Parallax

--- pythonProject/code/game.py
+++ pythonProject/code/game.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.level import Level
 from code.menu import Menu
 from const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 
@@ -16,8 +17,9 @@ class Game:
             menu = Menu(self.window)
             menu_return = menu.run()
 
-            if menu_return == MENU_OPTION[0]:
-                pass
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()  # Close Window
                 quit()  # end pygame
